f9a335f9097eafee46e62b04ffb3fec0dc644b17
zyshin <yeshuangzhu@gmail.com>
2016-12-16 10:45:14 +0800

    init

---
+++ provider.py
@@ -0,0 +1,248 @@
+# -*- coding: utf-8 -*-
+# @Author: yuchen
+# @Date:   2016-12-15 09:54:22
+# @Last Modified by:   yuchen
+# @Last Modified time: 2016-12-15 11:29:40
+
+from __future__ import print_function
+import numpy as np
+import sys
+import os
+import json
+import collections
+from os import path as op
+# from pyspin.spin import make_spin, Spin1
+# import showme
+
+
+class ptb_data_provider(object):
+
+    model_sample = {
+        's': {
+            'init_scale': 0.1,
+            'learning_rate': 1.0,
+            'max_grad_norm': 5,
+            'num_layers': 2,
+            'num_steps': 20,
+            'hidden_size': 200,
+            'max_epoch': 4,
+            'max_max_epoch': 13,
+            'keep_prob': 1.0,
+            'lr_decay': 0.5,
+            'batch_size': 128,
+            'vocab_size': 17000
+        },
+        'm': {
+            'init_scale': 0.05,
+            'learning_rate': 1.0,
+            'max_grad_norm': 5,
+            'num_layers': 2,
+            'num_steps': 35,
+            'hidden_size': 650,
+            'max_epoch': 6,
+            'max_max_epoch': 39,
+            'keep_prob': 0.5,
+            'lr_decay': 0.8,
+            'batch_size': 128,
+            'vocab_size': 17000
+        },
+        'l': {
+            'init_scale': 0.04,
+            'learning_rate': 1.0,
+            'max_grad_norm': 10,
+            'num_layers': 2,
+            'num_steps': 35,
+            'hidden_size': 1500,
+            'max_epoch': 14,
+            'max_max_epoch': 55,
+            'keep_prob': 0.35,
+            'lr_decay': 1. / 1.15,
+            'batch_size': 128,
+            'vocab_size': 17000
+        },
+        't': {
+            'init_scale': 0.1,
+            'learning_rate': 1.0,
+            'max_grad_norm': 1,
+            'num_layers': 1,
+            'num_steps': 2,
+            'hidden_size': 2,
+            'max_epoch': 1,
+            'max_max_epoch': 1,
+            'keep_prob': 1.0,
+            'lr_decay': 0.5,
+            'batch_size': 128,
+            'vocab_size': 17000
+        },
+    }
+
+    def __init__(self):
+        self.data_dir = ''
+
+        self.input_compat = raw_input if sys.version_info[0] < 3 else input
+        self.filenames = ['ptb.train.txt', 'ptb.valid.txt', 'ptb.test.txt', 'words_map.json']
+        self.data_dir = ''
+        self.model = ''
+        self.status = 'IDLE'
+
+        self.num_steps = 0
+        self.batch_size = 1
+        self.yield_pos = [0, 0, 0]
+
+        self._parse_config()
+        self._read_data()
+
+    def _parse_config(self):
+        self.legal_model_size = ['s', 'm', 'l', 't']
+        try:
+            assert op.isfile('config.json') and os.access('config.json', os.R_OK)
+            config = json.load(open('config.json', 'r'))
+            assert 'data_dir' in config and 'model' in config and 'threshold' in config and 'gpu_No' in config
+            self.gpu_No = "/gpu:" + str(config['gpu_No'])
+            self.data_dir = config['data_dir']
+            self.model = config['model']
+            self.threshold = config['threshold']
+            assert len(self.model) > 0
+            self.model = self.model[0].lower()
+            assert self.model in self.legal_model_size and op.isdir(self.data_dir) and os.access(self.data_dir, os.R_OK)
+            assert isinstance(self.threshold, int)
+            for filename in self.filenames:
+                fullname = op.join(self.data_dir, filename)
+                assert op.isfile(fullname) and os.access(fullname, os.R_OK)
+        except AssertionError:
+            self.data_dir = ''
+            self.model = ''
+            self.threshold = -1
+            self.gpu_No = 0
+            print ("Configure file load failed.")
+            cond = False
+            while not cond:
+                self.data_dir = self.input_compat("Please input data path: ")
+                cond, info = self._test_path()
+                print (info)
+            cond = False
+            while not cond:
+                self.model = self.input_compat("Please input model size(s/m/l/t): ")
+                cond, info = self._test_model()
+                print (info)
+            cond = False
+            while not cond:
+                self.threshold = self.input_compat("Please input word threshold: ")
+                cond, info = self._test_threshold()
+                print (info)
+        finally:
+            if os.access('config.json', os.R_OK) or (not op.exists('config.json') and os.access('.', os.R_OK)):
+                json.dump({
+                    'data_dir': self.data_dir,
+                    'model': self.model,
+                    'threshold': self.threshold,
+                    'gpu_No': 0
+                }, open('config.json', 'w'))
+            self.model_config = ptb_data_provider.model_sample[self.model]
+            self.batch_size = self.model_config['batch_size']
+            self.num_steps = self.model_config['num_steps']
+            print ("OK")
+
+    def _test_path(self):
+        if len(self.data_dir) == 0:
+            return False, "Data path length should be greater than 0."
+        elif not op.isdir(self.data_dir):
+            return False, "This path is not a directory."
+        elif not os.access(self.data_dir, os.R_OK):
+            return False, "This path is not accessible."
+        else:
+            for filename in self.filenames:
+                fullname = op.join(self.data_dir, filename)
+                if not op.isfile(fullname):
+                    return False, "File {} not found.".format(filename)
+                elif not os.access(fullname, os.R_OK):
+                    return False, "File {} not accessible"
+        return True, "Accepted"
+
+    def _test_model(self):
+        if len(self.model) == 0:
+            return False, "Model size length should be greater than 0."
+        self.model = self.model[0].lower()
+        if self.model not in self.legal_model_size:
+            return False, "Model size character not acceptable."
+        return True, "Accepted"
+
+    def _test_threshold(self):
+        if len(self.threshold) == 0:
+            return False, "Threshold should be an integer"
+        try:
+            if isinstance(self.threshold, str):
+                self.threshold = int(self.threshold)
+            if self.threshold < 0:
+                self.threshold = 0
+        except:
+            return False, "Threshold should be an integer"
+        return True, "Accepted"
+
+    def _read_data(self):
+        train_path = op.join(self.data_dir, self.filenames[0])
+        valid_path = op.join(self.data_dir, self.filenames[1])
+        test_path = op.join(self.data_dir, self.filenames[2])
+	vocab_path = op.join(self.data_dir, self.filenames[3])
+
+        decoder = lambda x: x.decode('utf-8').split()
+	self.vocab = json.load(open(vocab_path, 'r'))
+	self.vocab_nums = self.vocab.values()
+        with open(train_path, 'r') as training_source:
+            self.training_data = np.array(decoder(training_source.read()))
+            self.training_data = self.training_data[: (len(self.training_data) // self.batch_size) * self.batch_size].reshape((self.batch_size, len(self.training_data) // self.batch_size, ))
+
+        with open(valid_path, 'r') as valid_source:
+            self.valid_data = np.array(decoder(valid_source.read()))
+            self.valid_data = self.valid_data[: (len(self.valid_data) // self.batch_size) * self.batch_size].reshape((self.batch_size, len(self.valid_data) // self.batch_size, ))
+
+        with open(test_path, 'r') as test_source:
+            self.test_data = np.array(decoder(test_source.read()))
+            self.test_data = self.test_data[: (len(self.test_data) // self.batch_size) * self.batch_size].reshape((self.batch_size, len(self.test_data) // self.batch_size, ))
+
+    def get_config(self):
+        return self.model_config
+
+    def get_epoch_size(self):
+        if self.status == 'train':
+            return (self.training_data.shape[1]) // self.num_steps - 1
+        elif self.status == 'valid':
+            return (self.valid_data.shape[1]) // self.num_steps - 1
+        elif self.status == 'test':
+            return (self.test_data.shape[1]) // self.num_steps - 1
+        else:
+            return None
+
+    def __call__(self):
+        self.status = self.status.strip().lower()
+        if self.status == 'train':
+            self.yield_pos[0] = (self.yield_pos[0] + 1) % (self.training_data.shape[0] // self.batch_size)
+            epoch_size = (self.training_data.shape[1]) // self.num_steps - 1
+            for i in range(epoch_size):
+                x = self.training_data[self.yield_pos[0] * self.batch_size: (self.yield_pos[0] + 1) * self.batch_size, i * self.num_steps: (i + 1) * self.num_steps]
+                y = self.training_data[self.yield_pos[0] * self.batch_size: (self.yield_pos[0] + 1) * self.batch_size, i * self.num_steps + 1: (i + 1) * self.num_steps + 1]
+                yield (x, y)
+        elif self.status == 'valid':
+            self.yield_pos[1] = (self.yield_pos[1] + 1) % (self.valid_data.shape[0] // self.batch_size)
+            epoch_size = (self.valid_data.shape[1]) // self.num_steps - 1
+            for i in range(epoch_size):
+                x = self.valid_data[self.yield_pos[1] * self.batch_size: (self.yield_pos[1] + 1) * self.batch_size, i * self.num_steps: (i + 1) * self.num_steps]
+                y = self.valid_data[self.yield_pos[1] * self.batch_size: (self.yield_pos[1] + 1) * self.batch_size, i * self.num_steps + 1: (i + 1) * self.num_steps + 1]
+                yield (x, y)
+        else:
+            self.yield_pos[2] = (self.yield_pos[2] + 1) % (self.test_data.shape[0] // self.batch_size)
+            epoch_size = (self.test_data.shape[1]) // self.num_steps - 1
+            for i in range(epoch_size):
+                x = self.test_data[self.yield_pos[2] * self.batch_size: (self.yield_pos[2] + 1) * self.batch_size, i * self.num_steps: (i + 1) * self.num_steps]
+                y = self.test_data[self.yield_pos[2] * self.batch_size: (self.yield_pos[2] + 1) * self.batch_size, i * self.num_steps + 1: (i + 1) * self.num_steps + 1]
+                yield (x, y)
+
+if __name__ == "__main__":
+    '''
+    Debug
+    '''
+    provide = ptb_data_provider()
+    provide.status = 'train'
+    for x, y in provide():
+        print (x, y)
+	raw_input("Next")
---
+++ train.py
@@ -0,0 +1,226 @@
+# -*- coding: utf-8 -*-
+# @Author: yuchen
+# @Date:   2016-12-15 09:51:05
+# @Last Modified by:   yuchen
+# @Last Modified time: 2016-12-15 10:45:53
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import time
+
+import numpy as np
+import numpy.random as nr
+import tensorflow as tf
+import os
+
+# from pyspin.spin import make_spin, Spin1
+
+from provider import ptb_data_provider
+
+
+flags = tf.flags
+logging = tf.logging
+
+flags.DEFINE_bool("use_fp16", False,
+                  "Train using 16-bit floats instead of 32bit floats")
+
+FLAGS = flags.FLAGS
+provider = None
+config = None
+batch_size = 1024
+num_steps = 35
+size = 200
+vocab_size = 20000
+initial_state = None
+final_state = None
+
+X = None
+y = None
+
+cell = None
+
+cost = None
+train_op = None
+new_lr = None
+lr_update = None
+
+
+def data_type():
+    return tf.float16 if FLAGS.use_fp16 else tf.float32
+
+class PTBModel(object):
+    """The PTB model."""
+    def __init__(self, is_training, config):
+        self.batch_size = batch_size = config['batch_size']
+        self.num_steps = num_steps = config['num_steps']
+        size = config['hidden_size']
+        vocab_size = config['vocab_size']
+	# print ("Batch size = {batch_size}. Num steps = {num_steps}.".format(
+	# 	batch_size=batch_size, num_steps=num_steps))
+	# raw_input()
+        self._input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
+        self._targets = tf.placeholder(tf.int32, [batch_size, num_steps])
+
+        # Slightly better results can be obtained with forget gate biases
+        # initialized to 1 but the hyperparameters of the model would need to be
+        # different than reported in the paper.
+
+
+        with tf.device(config['gpu_No']):
+            lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(size, forget_bias=0.0, state_is_tuple=True)
+            if is_training and config['keep_prob'] < 1:
+            	lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=config['keep_prob'])
+            cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * config['num_layers'], state_is_tuple=True)
+
+        self._initial_state = cell.zero_state(batch_size, data_type())
+	with tf.device(config['gpu_No']):
+            embedding = tf.get_variable("embedding", [vocab_size, size], dtype=data_type())
+            inputs = tf.nn.embedding_lookup(embedding, self._input_data)
+
+        if is_training and config['keep_prob'] < 1:
+            inputs = tf.nn.dropout(inputs, config['keep_prob'])
+
+        # Simplified version of tensorflow.models.rnn.rnn.py's rnn().
+        # This builds an unrolled LSTM for tutorial purposes only.
+        # In general, use the rnn() or state_saving_rnn() from rnn.py.
+        #
+        # The alternative version of the code below is:
+        #
+        # inputs = [tf.squeeze(input_, [1])
+        #           for input_ in tf.split(1, num_steps, inputs)]
+        # outputs, state = tf.nn.rnn(cell, inputs, initial_state=self._initial_state)
+        outputs = []
+        state = self._initial_state
+        with tf.variable_scope("RNN"):
+            for time_step in range(num_steps):
+                if time_step > 0: tf.get_variable_scope().reuse_variables()
+                (cell_output, state) = cell(inputs[:, time_step, :], state)
+                outputs.append(cell_output)
+
+        output = tf.reshape(tf.concat(1, outputs), [-1, size])
+        softmax_w = tf.get_variable(
+            "softmax_w", [size, vocab_size], dtype=data_type())
+        softmax_b = tf.get_variable("softmax_b", [vocab_size], dtype=data_type())
+        logits = tf.matmul(output, softmax_w) + softmax_b
+        loss = tf.nn.seq2seq.sequence_loss_by_example(
+            [logits],
+            [tf.reshape(self._targets, [-1])],
+            [tf.ones([batch_size * num_steps], dtype=data_type())])
+        self._cost = cost = tf.reduce_sum(loss) / batch_size
+        self._final_state = state
+
+        if not is_training:
+            return
+
+    	self._lr = tf.Variable(0.0, trainable=False)
+    	tvars = tf.trainable_variables()
+    	grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars),
+                                      config['max_grad_norm'])
+    	optimizer = tf.train.GradientDescentOptimizer(self._lr)
+    	self._train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+    	self._new_lr = tf.placeholder(tf.float32, shape=[], name="new_learning_rate")
+    	self._lr_update = tf.assign(self._lr, self._new_lr)
+
+    def assign_lr(self, session, lr_value):
+        session.run(self._lr_update, feed_dict={self._new_lr: lr_value})
+    
+    @property
+    def input_data(self):
+        return self._input_data
+
+    @property
+    def targets(self):
+        return self._targets
+
+    @property
+    def initial_state(self):
+        return self._initial_state
+
+    @property
+    def cost(self):
+        return self._cost
+
+    @property
+    def final_state(self):
+        return self._final_state
+
+    @property
+    def lr(self):
+        return self._lr
+
+    @property
+    def train_op(self):
+        return self._train_op
+
+
+
+# @make_spin(Spin1, "Running epoch...")
+def run_epoch(session, model, provider, data, eval_op, verbose=False):
+    """Runs the model on the given data."""
+    start_time = time.time()
+    costs = 0.0
+    iters = 0
+    state = session.run(model.initial_state)
+    provider.status = data
+    for step, (x, y) in enumerate(provider()):
+        fetches = [model.cost, model.final_state, eval_op]
+        feed_dict = {}
+        feed_dict[model.input_data] = x
+        feed_dict[model.targets] = y
+        for i, (c, h) in enumerate(model.initial_state):
+            feed_dict[c] = state[i].c
+            feed_dict[h] = state[i].h
+	# print (feed_dict)
+    	cost, state, _ = session.run(fetches, feed_dict)
+    	costs += cost
+    	iters += model.num_steps
+
+    	epoch_size = provider.get_epoch_size()
+    	if verbose and step % (epoch_size // 10) == 10:
+            print("%.3f perplexity: %.3f speed: %.0f wps" %
+                (step * 1.0 / epoch_size, np.exp(costs / iters),
+             	iters * model.batch_size / (time.time() - start_time)))
+    return np.exp(costs / iters)
+
+
+def main():
+    os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    provider = ptb_data_provider()
+    provider.status = 'train'
+    config = provider.get_config()
+
+    eval_config = config.copy()
+    eval_config['batch_size'] = 1
+    eval_config['num_steps'] = 1
+
+    # print (config)
+    # print (eval_config)
+
+    with tf.Graph().as_default(), tf.Session() as session:
+        initializer = tf.random_uniform_initializer(-config['init_scale'], config['init_scale'])
+    	with tf.variable_scope("model", reuse=None, initializer=initializer):
+            m = PTBModel(is_training=True, config=config)
+    	with tf.variable_scope("model", reuse=True, initializer=initializer):
+            mvalid = PTBModel(is_training=False, config=config)
+            mtest = PTBModel(is_training=False, config=eval_config)
+
+    	tf.initialize_all_variables().run()
+
+    	for i in range(config['max_max_epoch']):
+            lr_decay = config['lr_decay'] ** max(i - config['max_epoch'], 0.0)
+            m.assign_lr(session, config['learning_rate'] * lr_decay)
+            print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
+
+            train_perplexity = run_epoch(session, m, provider, 'train', m.train_op, verbose=True)
+            print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
+            valid_perplexity = run_epoch(session, mvalid, provider, 'valid', tf.no_op())
+            print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
+
+    	test_perplexity = run_epoch(session, mtest, provider, 'test', tf.no_op())
+    	print("Test Perplexity: %.3f" % test_perplexity)
+
+if __name__ == "__main__":
+    main()
