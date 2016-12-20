c1a1146c3ab23b05de9a8c322bf19eaf31048513
潘星宇 <421915293@163.com>
2016-12-20 20:27:52 +0800

    change preOperation

--- preOperation.py
+++ preOperation.py
@@ -12,10 +12,12 @@ parser.add_argument('input', type=str)
 parser.add_argument('--output_dir', type=str, default='./filtered_%s/' % str(now))
 parser.add_argument('--threshold', type=int, default=100)
 parser.add_argument('--percent', type=int, default=10)
+parser.add_argument('--only_filter', type=int, default=0)
 args = parser.parse_args()
 
 input_file = args.input
 threhold = args.threshold
+only_filter_flag = args.only_filter;
 
 word_map = {}
 line_num = 0
@@ -60,70 +62,76 @@ for line in lines:
   #   print(".", end=" ")
   lines[line_num] = " ".join(char_list).encode("utf-8")
   line_num += 1
-word_map["{{R}}"] = 0
-word_map_int = {}
-No = 0
-for word, num in word_map.items():
-  word_map_int[word] = No
-  No += 1
-word_map_int["{{vocab_size}}"] = len(word_map)
-if not args.output_dir in os.listdir("./"):
-  os.mkdir(args.output_dir)
-with open(os.path.join(args.output_dir, 'words_map.json'), 'w') as f_word_map:
-  f_word_map.write(json.dumps(word_map_int))
-print("--------Finish file filtering-------")
+if(only_filter_flag == 1):
+  with open(os.path.join(args.output_dir, 'filter.txt'), 'w') as output:
+    for line in lines:
+      output.write(line + "\n")
+  print("--------Finish file filtering-------")
+else: 
+  word_map["{{R}}"] = 0
+  word_map_int = {}
+  No = 0
+  for word, num in word_map.items():
+    word_map_int[word] = No
+    No += 1
+  word_map_int["{{vocab_size}}"] = len(word_map)
+  if not args.output_dir in os.listdir("./"):
+    os.mkdir(args.output_dir)
+  with open(os.path.join(args.output_dir, 'words_map.json'), 'w') as f_word_map:
+    f_word_map.write(json.dumps(word_map_int))
+  print("--------Finish file filtering-------")
 
-print("--------Start file sampling-------")
-sample_lines = list(filter(lambda x: nr.random() * 100.0 <= args.percent, lines))
-train = []
-valid = []
-test = []
-for line in sample_lines:
-  tmp = nr.random()
-  if tmp <= 0.75:
-      train.append(line)
-  elif tmp <= 0.85:
-      valid.append(line)
-  else:
-      test.append(line)
-lines = []
-line_num = 0
-for line in train:
-  content = line.strip('\n').decode("utf-8")
-  char_list = content.split(" ")
-  for i in range(len(char_list)):
-    if(char_list[i] == ""):
-      continue
-    char_list[i] = str(word_map_int[char_list[i]])
-  train[line_num] = " ".join(char_list).encode("utf-8") + "\n"
-  line_num += 1
+  print("--------Start file sampling-------")
+  sample_lines = list(filter(lambda x: nr.random() * 100.0 <= args.percent, lines))
+  train = []
+  valid = []
+  test = []
+  for line in sample_lines:
+    tmp = nr.random()
+    if tmp <= 0.75:
+        train.append(line)
+    elif tmp <= 0.85:
+        valid.append(line)
+    else:
+        test.append(line)
+  lines = []
+  line_num = 0
+  for line in train:
+    content = line.strip('\n').decode("utf-8")
+    char_list = content.split(" ")
+    for i in range(len(char_list)):
+      if(char_list[i] == ""):
+        continue
+      char_list[i] = str(word_map_int[char_list[i]])
+    train[line_num] = " ".join(char_list).encode("utf-8") + "\n"
+    line_num += 1
 
-line_num = 0
-for line in test:
-  content = line.strip('\n').decode("utf-8")
-  char_list = content.split(" ")
-  for i in range(len(char_list)):
-    if(char_list[i] == ""):
-      continue
-    char_list[i] = str(word_map_int[char_list[i]])
-  test[line_num] = " ".join(char_list).encode("utf-8") + "\n"
-  line_num += 1
+  line_num = 0
+  for line in test:
+    content = line.strip('\n').decode("utf-8")
+    char_list = content.split(" ")
+    for i in range(len(char_list)):
+      if(char_list[i] == ""):
+        continue
+      char_list[i] = str(word_map_int[char_list[i]])
+    test[line_num] = " ".join(char_list).encode("utf-8") + "\n"
+    line_num += 1
 
-line_num = 0
-for line in valid:
-  content = line.strip('\n').decode("utf-8")
-  char_list = content.split(" ")
-  for i in range(len(char_list)):
-    if(char_list[i] == ""):
-      continue
-    char_list[i] = str(word_map_int[char_list[i]])
-  valid[line_num] = " ".join(char_list).encode("utf-8") + "\n"
-  line_num += 1
+  line_num = 0
+  for line in valid:
+    content = line.strip('\n').decode("utf-8")
+    char_list = content.split(" ")
+    for i in range(len(char_list)):
+      if(char_list[i] == ""):
+        continue
+      char_list[i] = str(word_map_int[char_list[i]])
+    valid[line_num] = " ".join(char_list).encode("utf-8") + "\n"
+    line_num += 1
 
-with open(os.path.join(args.output_dir, 'ptb.train.txt'), 'w') as ftrain:
-  ftrain.writelines(train)
-with open(os.path.join(args.output_dir, 'ptb.valid.txt'), 'w') as fvalid:
-  fvalid.writelines(valid)
-with open(os.path.join(args.output_dir, 'ptb.test.txt'), 'w') as ftest:
-  ftest.writelines(test)
+  with open(os.path.join(args.output_dir, 'ptb.train.txt'), 'w') as ftrain:
+    ftrain.writelines(train)
+  with open(os.path.join(args.output_dir, 'ptb.valid.txt'), 'w') as fvalid:
+    fvalid.writelines(valid)
+  with open(os.path.join(args.output_dir, 'ptb.test.txt'), 'w') as ftest:
+    ftest.writelines(test)
 print ("Completed!")
